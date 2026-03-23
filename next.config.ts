25e56949401a8767c9d6ae14cbcbda94b712a1c1
Emmanuel Anene-Maidoh <anenemaidohemmanuel@gmail.com>
2026-03-23 22:44:33 +0100

    done

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // Set the absolute path to your project root
+    root: path.join(__dirname),
+  },
 };
 
 export default nextConfig;
